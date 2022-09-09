b3f5e6e0a3b0db7dc3ebf7da8cbf80eedd1665ca
helenjfr <helenjfr@gmail.com>
2022-09-09 21:46:18 +0430

    Delete Products Added

--- WebApplication3/Controllers/ProductController.cs
+++ WebApplication3/Controllers/ProductController.cs
@@ -4,11 +4,12 @@ using WebApplication5.Models;
 using Car = WebApplication5.Models.Car;
 
 namespace WebApplication3.Controllers;
+
 using Microsoft.AspNetCore.Mvc;
 
 [ApiController]
 [Route("[Controller]")]
-public class ProductController: ControllerBase
+public class ProductController : ControllerBase
 {
     public databaseContext context;
 
@@ -16,17 +17,17 @@ public class ProductController: ControllerBase
     {
         context = contextCopy;
     }
+
     [HttpGet]
     public async Task<ActionResult<List<Product>>> GetAllProducts()
     {
         var ProductsFromDatabase = await context.Products.ToListAsync();
         return ProductsFromDatabase;
-
     }
 
     // [HttpGet("GetSpecificProduct")]
     // {
-        
+
     // }
     [HttpPost]
     public async Task<ActionResult<Product>> creatNewProduct(Product NewProduct)
@@ -42,22 +43,30 @@ public class ProductController: ControllerBase
         // var ProductsFromDatabase = await context.Products.ToListAsync();
         // foreach (var product in ProductsFromDatabase)
         // {
-            // if (product.id == ProductId)
-            // {
-                // return Ok(product);
-            // }
-            var FoundProduct = await context.Products.FirstAsync(product => (product.id == ProductId));
-            if (FoundProduct == null)
-            {
-                return NotFound();
-            }
-            else
-            {
-                return Ok(FoundProduct);
-            }
+        // if (product.id == ProductId)
+        // {
+        // return Ok(product);
+        // }
+        var FoundProduct = await context.Products.FirstAsync(product => (product.id == ProductId));
+        if (FoundProduct == null)
+        {
             return NotFound();
+        }
+        return Ok(FoundProduct);
     }
 
-        
-    
+    [HttpDelete("{ProductId}")]
+    public async Task<ActionResult> DeleteProduct(int ProductId)
+    {
+        var FoundProduct = await context.Products.FirstAsync(product => (product.id == ProductId));
+        if (FoundProduct == null)
+        {
+            return NotFound("Existential crisis");
+        }
+
+        context.Products.Remove(FoundProduct);
+        await context.SaveChangesAsync();
+        return Ok();
+    }
+
 }
